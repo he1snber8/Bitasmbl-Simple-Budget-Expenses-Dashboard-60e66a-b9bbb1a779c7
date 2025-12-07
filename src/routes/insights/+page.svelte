40e8f4a8ed1f655625b9917b767567bd9d74abf1
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-12-07 15:50:29 +0400

    Add +page.svelte

---
+++ src/routes/insights/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import SpendingChart from '$lib/components/SpendingChart.svelte';
+  import IncomeExpenseSummary from '$lib/components/IncomeExpenseSummary.svelte';
+</script>
+
+<section>
+  <h2>Spending Insights</h2>
+  <IncomeExpenseSummary />
+  <SpendingChart />
+</section>
