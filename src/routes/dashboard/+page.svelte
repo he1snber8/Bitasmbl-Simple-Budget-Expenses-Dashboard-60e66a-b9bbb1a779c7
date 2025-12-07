35a75c908b2262acb2c80106a1da244fc76c8cea
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-12-07 15:50:26 +0400

    Add +page.svelte

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import EntryForm from '$lib/components/EntryForm.svelte';
+  import EntryList from '$lib/components/EntryList.svelte';
+</script>
+
+<section>
+  <h2>Entries</h2>
+  <EntryForm />
+  <EntryList />
+</section>
