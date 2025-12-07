2a89b4f42c7ff5d055573e3de4d71d0581538388
he1snber8 <116442582+he1snber8@users.noreply.github.com>
2025-12-07 15:50:20 +0400

    Add +page.svelte

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let now = new Date();
+  const month = now.toLocaleString('default',{month:'long'});
+  const year = now.getFullYear();
+</script>
+
+<section>
+  <h2>{month} {year} Overview</h2>
+  <p>Dashboard coming soon...</p>
+</section>
